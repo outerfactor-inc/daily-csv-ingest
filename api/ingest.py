670768ec5702e0c157800c3704c66c00bfee0ea3
outerfactor-inc <tarin@outerfactor.com>
2026-01-29 16:18:43 -0500

    Refactor handler to use BaseHTTPRequestHandlerchnage

--- api/ingest.py
+++ api/ingest.py
@@ -1,6 +1,8 @@
-def handler(request):
-    return {
-        "statusCode": 200,
-        "headers": {"content-type": "text/plain"},
-        "body": "OK"
-    }
+from http.server import BaseHTTPRequestHandler
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write(b"OK")
