a517efdcef7b18c7a27474d8a29e34b492ab3532
outerfactor-inc <tarin@outerfactor.com>
2026-02-02 22:32:02 +0000

    fixed preview file

--- api/sell_through/three_eye/sf_preview.py
+++ api/sell_through/three_eye/sf_preview.py
@@ -6,7 +6,7 @@ from http.server import BaseHTTPRequestHandler
 from urllib.parse import urlparse, parse_qs
 
 from api.sell_through.three_eye.source import get_latest_csv_snapshot
-from api.sell_through.three_eye.csv_parser import parse_sell_through_csv  # <-- your sell-through parser
+from api.sell_through.three_eye.csv_parser import parse_sell_through_3e_csv  # <-- your sell-through parser
 from api.sell_through.three_eye.sf_upsert import build_sell_through_fields, build_sell_through_line_fields
 
 def group_by_transaction(rows: list[dict]) -> dict[str, list[dict]]:
