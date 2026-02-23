5fa05315683f90cfe58cbc8081950b388203fd0c
outerfactor-inc <tarin@outerfactor.com>
2026-02-23 14:36:17 -0500

    Update source.py

--- api/sell_through/ab/source.py
+++ api/sell_through/ab/source.py
@@ -1,11 +1,20 @@
 import os
-from api.shared.graph_mail import find_latest_message_with_attachment
+from api.shared.graph_mail_base import get_ms_token, get_attachment_bytes
+from api.sell_through.ab.email_filter import find_latest_ab_sell_through_email
 
 def get_latest_csv_snapshot() -> dict:
-    return find_latest_message_with_attachment(
-        mailbox_user=os.environ["MAILBOX_USER"],
-        subject_contains=os.environ.get("SELL_THROUGH_MAIL_SUBJECT_AB"),
-        from_email=os.environ.get("SELL_THROUGH_MAIL_FROM_AB"),
-        attachment_name_contains=os.environ.get("SELL_THROUGH_ATTACHMENT_NAME_CONTAINS", ".csv"),
-        max_messages=int(os.environ.get("SELL_THROUGH_MAX_MESSAGES", "100")),
-    )
+    token = get_ms_token()
+
+    snap = find_latest_ab_sell_through_email(token)
+    if not snap.get("matched"):
+        return snap
+
+    mailbox = snap["filters"]["MAILBOX_USER"]
+    msg_id = snap["pickedMessage"]["id"]
+    att_id = snap["attachment"]["id"]
+
+    csv_bytes = get_attachment_bytes(token, mailbox, msg_id, att_id)
+
+    snap["csv_bytes"] = csv_bytes
+    snap["csv_bytes_len"] = len(csv_bytes)
+    return snap
