898aa8c37bd4ee83027df43d933bc9ad5337ed07
Tarin Nurany <tarin@Tarins-MacBook-Pro.local>
2026-02-23 16:48:48 -0500

    ship date fix

--- api/sell_through/ab/email_filter.py
+++ api/sell_through/ab/email_filter.py
@@ -20,7 +20,7 @@ def find_latest_ab_sell_through_email(token: str) -> Dict[str, Any]:
     subject_contains = _norm(os.environ.get("SELL_THROUGH_MAIL_SUBJECT_AB"))
     from_filter = _norm(os.environ.get("SELL_THROUGH_MAIL_FROM_AB"))
     att_name_contains = _norm(os.environ.get("ATTACHMENT_NAME_CONTAINS_AB", ".xls"))
-    top = int(os.environ.get("SELL_THROUGH_AB_MAX_MESSAGES", "500"))
+    top = int(os.environ.get("SELL_THROUGH_AB_MAX_MESSAGES", "50"))
 
     messages = list_recent_messages(token, mailbox, top=top)
 
@@ -86,5 +86,5 @@ def find_latest_ab_sell_through_email(token: str) -> Dict[str, Any]:
             "MAIL_FROM": from_filter or None,
             "ATTACHMENT_NAME_CONTAINS": att_name_contains or None,
         },
-        "subjects_preview": [m.get("subject") for m in messages[:100]],
+        "subjects_preview": [m.get("subject") for m in messages[:10]],
     }
